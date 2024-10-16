d3fcde129c0b849e4ac1351f82c0d5807112bd6f
Diego <diegoinsano156@gmail.com>
2024-10-16 16:12:38 +0200

    Version 3 borre los comentarios y puse un comentario explicativo del codigo

--- codigo.cpp
+++ codigo.cpp
@@ -1,21 +1,17 @@
-
+// PRIMERO DECLARE A B Y C PARA PODER HACER LAS OPERACIONES Y DECLARE CON DOUBLE LAS VARIABLES D, X1, X2 PARA QUE LOS RESULTADOS PUEDAN SALIR EN DECIMAL LUEGO LEI A B Y C Y DESPUES HICE LA FORMULA PARA PODER HACER LA RAIZ Y AL FINAL PEDI EL VALOR DE x1 Y X2
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
     int A, B, C;
-    double D, x1, x2; // Usamos double para las raíces
+    double D, x1, x2;
     cout<<"CODIGO CREADO POR DIEGO DE 3RO DE PROGRAMACION";
 
-    // Leer los coeficientes
     cin >> A >> B >> C;
 
-    // Calcular el discriminante
     D = (B * B) - (4 * A * C);
 
     x1 = (-B + sqrt(D)) / (2 * A);
     x2 = (-B - sqrt(D)) / (2 * A);
-
-    // Mostrar las raíces
 
     cout << x1 << " " << x2 << endl;
 
