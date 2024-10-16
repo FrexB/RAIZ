42e5c689a3993da47f550467526d9225d3f277fd
Diego <diegoinsano156@gmail.com>
2024-10-16 16:08:13 +0200

    Version 2 Agregue los comentarios

--- codigo.cpp
+++ codigo.cpp
@@ -3,17 +3,19 @@
 using namespace std;
 int main() {
     int A, B, C;
-    double D, x1, x2;
+    double D, x1, x2; // Usamos double para las raíces
     cout<<"CODIGO CREADO POR DIEGO DE 3RO DE PROGRAMACION";
 
-
+    // Leer los coeficientes
     cin >> A >> B >> C;
 
-
+    // Calcular el discriminante
     D = (B * B) - (4 * A * C);
 
     x1 = (-B + sqrt(D)) / (2 * A);
     x2 = (-B - sqrt(D)) / (2 * A);
+
+    // Mostrar las raíces
 
     cout << x1 << " " << x2 << endl;
 
